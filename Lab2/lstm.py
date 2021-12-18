7f456fd166c4858022ee3a1cd560d7fb2753bf5e
JasonChatzitheodorou <chatzitheodorou99@gmail.com>
2021-12-18 13:21:00 +0000

    Started step 14 and corrected one of the previous

---
+++ Lab2/lstm.py
@@ -0,0 +1,115 @@
+import os
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+import torch.nn as nn
+
+
+class FrameLevelDataset(Dataset):
+    def __init__(self, feats, labels):
+        """
+            feats: Python list of numpy arrays that contain the sequence features.
+                   Each element of this list is a numpy array of shape seq_length x feature_dimension
+            labels: Python list that contains the label for each sequence (each label must be an integer)
+        """
+        self.lengths =  np.array([x.shape[0] for x in feats]) # Find the lengths 
+
+        self.feats = self.zero_pad_and_stack(feats)
+        if isinstance(labels, (list, tuple)):
+            self.labels = np.array(labels).astype('int64')
+
+    def zero_pad_and_stack(self, x):
+        """
+            This function performs zero padding on a list of features and forms them into a numpy 3D array
+            returns
+                padded: a 3D numpy array of shape num_sequences x max_sequence_length x feature_dimension
+        """
+        padded = []
+        
+        # --------------- Insert your code here ---------------- #
+        max_length = self.lengths.max()
+        for sample in x:
+            padded.append(np.pad(sample, [(0, max_length - sample.shape[0]), (0, 0)], 'constant'))
+
+        return np.array(padded)
+
+    def __getitem__(self, item):
+        return self.feats[item], self.labels[item], self.lengths[item]
+
+    def __len__(self):
+        return len(self.feats)
+
+class extractfirst(nn.Module):
+    def forward(self,x):
+        out , _ = x
+        return out
+
+class BasicLSTM(nn.Module):
+    def __init__(self, input_dim, rnn_size, output_dim, num_layers, bidirectional=False, dropout=0):
+        super(BasicLSTM, self).__init__()
+        self.bidirectional = bidirectional
+        self.feature_size = rnn_size * 2 if self.bidirectional else rnn_size
+        self.num_layers = num_layers
+        self.rnn_size = rnn_size
+        self.output_dim = output_dim
+        self.dropout = dropout
+
+        # --------------- Insert your code here ---------------- #
+        # Initialize the LSTM, Dropout, Output layers
+        self.lstm = nn.LSTM(input_dim, self.rnn_size, self.num_layers, batch_first=True, bidirectional=self.bidirectional, dropout=self.dropout)
+        self.linear = nn.Linear(self.feature_size, self.output_dim)
+
+    def forward(self, x, lengths):
+        """ 
+            x : 3D numpy array of dimension N x L x D
+                N: batch index
+                L: sequence index
+                D: feature index
+
+            lengths: N x 1
+         """
+        
+        # --------------- Insert your code here ---------------- #
+        
+        # You must have all of the outputs of the LSTM, but you need only the last one (that does not exceed the sequence length)
+        # To get it use the last_timestep method
+        # Then pass it through the remaining network
+        if self.bidirectional:
+            h0 = torch.zeros(self.num_layers*2, x.size(0), self.rnn_size)
+            c0 = torch.zeros(self.num_layers*2, x.size(0), self.rnn_size)
+        else:
+            h0 = torch.zeros(self.num_layers, x.size(0), self.rnn_size)
+            c0 = torch.zeros(self.num_layers, x.size(0), self.rnn_size)
+            
+        lstm_out, _ = self.lstm(x, (h0, c0))
+        last_outputs = self.linear(self.last_timestep(lstm_out, lengths, self.bidirectional))
+        
+        return last_outputs
+
+    def last_timestep(self, outputs, lengths, bidirectional=False):
+        """
+            Returns the last output of the LSTM taking into account the zero padding
+        """
+        if bidirectional:
+            forward, backward = self.split_directions(outputs)
+            last_forward = self.last_by_index(forward, lengths)
+            last_backward = backward[:, 0, :]
+            # Concatenate and return - maybe add more functionalities like average
+            return torch.cat((last_forward, last_backward), dim=-1)
+
+        else:
+            return self.last_by_index(outputs, lengths)
+
+    @staticmethod
+    def split_directions(outputs):
+        direction_size = int(outputs.size(-1) / 2)
+        forward = outputs[:, :, :direction_size]
+        backward = outputs[:, :, direction_size:]
+        return forward, backward
+
+    @staticmethod
+    def last_by_index(outputs, lengths):
+        # Index of the last output for each sequence.
+        idx = (lengths - 1).view(-1, 1).expand(outputs.size(0),
+                                               outputs.size(2)).unsqueeze(1)
+        return outputs.gather(1, idx).squeeze()
